27ac26f1d21d1454ba418018f33ccda0c5df7bf4
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-09-01 17:04:52 -0300

    Movendo 'fortune cookie' para outro modulo

--- meadowlark.js
+++ meadowlark.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
+const fortune = require('./lib/fortune.js')
 
 const app = express()
-const fortunes = [
-    'Conquer your fears or they will conquer you.',
-    'Rivers need springs.',
-    'Do not fear what you don\'t know.',
-    'You will have a pleasant surprise.',
-    'Whenever possible, keep it simple.',
-]
+
 app.engine('handlebars', engine({
     defaultLayout: 'main',
 }))
@@ -24,7 +19,7 @@ app.get('/', (req, res) => res.render('home'))
 
 app.get('/about', (req, res) => {
     const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
-    res.render('about', { fortune: randomFortune })
+    res.render('about', { fortune: fortune.getFortune() })
 })
 
 //pagina 404 personalizada
